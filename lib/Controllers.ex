109a4a90a3340ed31686d38566886b01fb398c8e
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-08-06 17:08:46 -0500

    some cool new stuf
    i added  a  template for the publications , a  new route and some stuff

--- lib/Controllers.ex
+++ lib/Controllers.ex
@@ -1,7 +1,21 @@
 defmodule Anonpost.Controllers do
   @spec check404Files(binary) :: binary
+  @template_dir  "templates"
+  def boards(), do:  ["haskell","elixir","github","programming","bruh boys"]
+
+  def isOnBoards?(board) ,do: Enum.member?(boards(),board)
+
 
   def check404Files(path) do
     if File.exists?(path), do: path, else: "view/404.html"
+  end
+  def render(%{status: status} = conn, template, assigns \\ []) do
+    body =
+      @template_dir
+      |> Path.join(template)
+      |> String.replace_suffix(".html", ".html.eex")
+      |> EEx.eval_file(assigns)
+
+    Plug.Conn.send_resp(conn, (status || 200), body)
   end
 end
--- lib/Router.ex
+++ lib/Router.ex
@@ -1,20 +1,38 @@
 defmodule Anonpost.Router do
   use Plug.Router
-
+  alias Plug.Conn.Query, as: QeryStr
   plug(:match)
   plug(:dispatch)
+
+  plug(Plug.Parsers,
+    parsers: [:urlencoded, :multipart],
+    pass: ["text/*"]
+  )
+
   use Plug.ErrorHandler
+
   get "/" do
+
     conn
     |> put_resp_content_type("text/html")
     |> send_file(200, "./view/home.html")
   end
 
   get "/public/*_file" do
-    conn |> send_file(200,Anonpost.Controllers.check404Files( "." <> conn.request_path))
+    conn |> send_file(200, Anonpost.Controllers.check404Files("." <> conn.request_path))
   end
 
+  get "/board" do
+    params = QeryStr.decode(conn.query_string)
+    board=params["board"]
+    isOn = Anonpost.Controllers.isOnBoards?(board)
 
+    if isOn do
+      Anonpost.Controllers.render(conn,"boards.eex",[board_title: board])
+    else
+      send_file(conn, 404, "./view/404.html")
+    end
+  end
 
   match _ do
     conn |> send_file(404, "./view/404.html")
