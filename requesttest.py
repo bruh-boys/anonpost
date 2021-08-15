980da9551710642b24875190074c4ad1822440d5
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-08-15 02:42:24 -0500

    deleting some trash

--- requesttest.py
+++
@@ -1,7 +0,0 @@
-import requests
-# im using this simple file for test the requests of the service
-# its a little bit shitty i know 
-r=requests.post("http://localhost:8080/board")
-formvalues={"username":"test","title":"test","body":"test"}
-
-r=requests.post("http://localhost:8080/board",formvalues)
